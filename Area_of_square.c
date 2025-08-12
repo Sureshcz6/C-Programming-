d544a5695bfe7aa35c07ec8beeafe4da9d1f740a
SureshCz6 <skmahto285@gmail.com>
2025-08-12 20:52:53 +0530

    Area of Square

---
+++ Area_of_square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+        int square,Area_Square;
+        printf("Enter the value of Square: ");
+        scanf("%d",&square);
+        Area_Square=square*square;
+        printf("THE VALUE OF AREA OF SQUARE:  %d :",Area_Square);
+        return 0;
+
+    }
