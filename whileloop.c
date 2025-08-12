81d6c1231d995a9adfd0905721b2598d46b369e9
SureshCz6 <skmahto285@gmail.com>
2025-08-12 20:35:38 +0530

    while loop
    while Loop Simple Programms

---
+++ whileloop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+    {
+        int user;
+      
+        printf("Enter User of Number : ");
+        scanf("%d",&user);
+          int i=0;
+        while(i<=user)
+           {
+                printf("%d\n",i);
+              
+              
+                printf(" Hello World ");
+                  i++;
+            }
+    return 0;
+    }
