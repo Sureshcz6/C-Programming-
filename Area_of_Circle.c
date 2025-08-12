3b909ac6ea9dd142ebb64a49ff66057eaf337894
SureshCz6 <skmahto285@gmail.com>
2025-08-12 20:49:34 +0530

    Area of Cirlce find using C Programming

---
+++ Area_of_Circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+    {
+        float radius;
+        printf("Enter the Circle of Radius");
+        scanf("%f",&radius);
+        float Cicle=3.14*radius*radius;
+        printf("THE AREA OF CIRCLE %f",Cicle);
+        return 0;
+    }
