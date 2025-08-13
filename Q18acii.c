6cf289b51019c595a0c1f3f41500ea5173133443
SureshCz6 <skmahto285@gmail.com>
2025-08-13 12:28:51 +0530

    ASCII VALUE PRINT

---
+++ Q18acii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()          
+// print all ascii character 
+    {
+        char i;
+        for(i=33;i<=126;i++)
+            {
+                printf("%c  ",i);
+            }
+        return 0;
+    }
