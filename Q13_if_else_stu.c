8f7ca0fece9c573c64b4357b69a08be3075476a7
SureshCz6 <skmahto285@gmail.com>
2025-08-13 10:26:10 +0530

    Student Pass or Fail using C

---
+++ Q13_if_else_stu.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+    {
+        int marks;
+        printf("ENTER MARKS OF STUDENTS:(0-100) ");
+        scanf("%d",&marks);
+        if(marks>30)
+            {
+                printf("this student are the PASS");
+            }
+        else    {
+            printf("FAIL");
+        }
+    printf("\n THANKS YOU FOR USES THAT SOFTWARE");
+        return 0;
+    }
