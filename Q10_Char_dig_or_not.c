1c6b8b97380abbd50e6d35b0798a8cebc8f15fb5
SureshCz6 <skmahto285@gmail.com>
2025-08-13 10:19:20 +0530

    check character digit using C

---
+++ Q10_Char_dig_or_not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+         int User_char;
+         printf("Input a character then check (charcter or digit)");
+         scanf("%d",&User_char);
+         printf("character are %d",((User_char>=65)||(User_char>=122)));
+        // printf("%d",User_char==1||User_char==2||User_char==3||User_char==4||User_char==5||User_char==6||User_char==7||User_char==8||User_char==9||User_char==0);
+        // printf("%",User_char=
+        return 0;
+    }
