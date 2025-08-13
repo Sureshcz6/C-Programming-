a1c0c3253288670648053181aae32ba8acc04085
SureshCz6 <skmahto285@gmail.com>
2025-08-13 11:54:32 +0530

    strcmp (two string compare) method

---
+++ strcmp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+    {
+        char firststr[]="Apple";
+        char secondstr[]="Banana";
+        printf("%d",strcmp(firststr,secondstr));
+        puts(firststr);
+        puts(secondstr);
+        return 0;
+    }
